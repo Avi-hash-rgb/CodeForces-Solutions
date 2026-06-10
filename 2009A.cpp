b5db86b8a919ebb1b670060428622a87baccc754
Prayag Awasthi <awasthiprayag044@gmail.com>
2026-06-10 22:11:28 +0530

    Create 2009A.cpp

---
+++ 2009A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+int32_t main() {
+    fastio;
+    int t; cin >> t;
+    while(t--){
+        int a, b; cin >> a >> b;
+        cout << b - a << endl;
+    }
+}
