60a7103e9e23e18cdce77f5139b845fba8a8b72e
Prayag Awasthi <awasthiprayag044@gmail.com>
2026-06-11 22:02:50 +0530

    Update 1475A.cpp

--- 1475A.cpp
+++ 1475A.cpp
@@ -7,11 +7,8 @@ int main(){
 
     int t; cin >> t;
     while(t--){
-        int n; cin >> n;
-        if(n % 2 != 0) cout << "YES" << endl;
-        else{
-            if(n % 2 == 0 && n % 3 == 0) cout << "YES" << endl;
-            else cout << "NO" << endl;
-        }
+        long long n; cin >> n;
+        if(n > 0 && ((n & (n - 1)) == 0)) cout << "NO\n";
+        else cout << "YES\n";
     }
 }
